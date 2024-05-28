53879a6da7c2a848507db395fa7df1a2c97a7ff0
anael <leinert.anael@gmail.com>
2024-05-28 14:58:38 +0200

    chore: add file for setting make variables

---
+++ set_vars.sh
@@ -0,0 +1,4 @@
+export CUDA_HOME=/usr/local/cuda-12.3
+export CUDA_PATH=/usr/local/cuda-12.3
+export CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-12.3
+export CMAKE_CUDA_COMPILER=/usr/local/cuda-12.3/bin/nvcc
